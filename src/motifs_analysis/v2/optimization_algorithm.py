63e86f7655662894683a62336f3fdbc7ab903dab
Soumajyoti Sarkar <sarkar.soumajyoti@gmail.com>
2017-11-15 16:14:38 -0700

    Minimize netwon

--- src/motifs_analysis/v2/optimization_algorithm.py
+++ src/motifs_analysis/v2/optimization_algorithm.py
@@ -5,6 +5,42 @@ from sklearn import linear_model
 from scipy.optimize import minimize
 import random
 
+degDict = pd.read_pickle('../../../data/deg_centralities_diff_T07_08-v1.pcikle')
+inputDf = pd.read_pickle('../../../data/df_optimize_input_sample.pickle')
+midList = list(set(inputDf['mid']))[:10]
+
+inputDf = inputDf[inputDf['mid'].isin(midList)]
+
+outDeg, inDeg = degDict
+count_pairs_alpha = 0
+nodePairs_alpha = {}
+
+count_pairs_eta = 0
+nodePairs_eta = {}
+# Keep the list of unique source-target pairs for parameter retrieval
+for idx, row in inputDf.iterrows():
+    currNode = row['node']
+    parent = row['parents']
+    nonParents = row['nonParents']
+    exposedNodes = row['exposedNodes']
+
+    # For alpha parameter
+    if (currNode, parent) not in nodePairs_alpha:
+        nodePairs_alpha[(currNode, parent)] = count_pairs_alpha
+        count_pairs_alpha += 1
+
+    for k in nonParents:
+        if (currNode, k) not in nodePairs_alpha:
+            nodePairs_alpha[(currNode, k)] = count_pairs_alpha
+            count_pairs_alpha += 1
+
+    # For eta paramter
+    for k in exposedNodes:
+        if (currNode, k) not in nodePairs_eta:
+            nodePairs_eta[(currNode, k)] = count_pairs_eta
+            count_pairs_eta += 1
+
+
 def binarySearch(alist, item):
     first = 0
     last = len(alist)-1
@@ -40,13 +76,11 @@ def lasso_solve(X, y):
     regr.fit(X, y)
 
 
-def G_1(inputDf, alpha, eta):
+def G_1_alpha(inputDf, alpha):
     '''
 
     :param inputDf: Dataframe containing the node information for each node
     :param: alpha: transmission rate
-    :param: eta: exposure rate
-    :param: degDict: degree dictionary of nodes
     :return:
     '''
 
@@ -59,12 +93,13 @@ def G_1(inputDf, alpha, eta):
     # Compute the log likelihood for each cascade
     last = 0.
     survival = 0.
-    exposure = 0.
     hazard = 0.
     curr_cascade_funcValue = 0.
+
     for idx, row in inputDf.iterrows():
+        currNode = row['node']
+        parent = row['parents']
         nonParents = row['nonParents']
-        exposureNodes = row['exposedNodes']
 
         t_i = row['time']
 
@@ -77,20 +112,48 @@ def G_1(inputDf, alpha, eta):
                 t_k = last - 1
             else:
                 t_k = nodeRtTime[k]
-
-            survival += (-alpha[idx] * (np.power(t_i - t_k, 2) * 0.5))
-
+            survival += (-alpha[nodePairs_alpha[(currNode, k)]] * (np.power(t_i - t_k, 2) * 0.5))
 
         # 2. Hazard term
-        j = row['parents']
+        j = parent
         if j not in nodeRtTime:
             t_j = last - 1
         else:
             t_j = nodeRtTime[j]
+        hazard = np.log(alpha[nodePairs_alpha[(currNode, j)]] * abs(t_i - t_j))
+
+        last = row['time']
+
+        # Add the log terms
+        curr_cascade_funcValue += survival + hazard
+        func_LL += curr_cascade_funcValue
+
+    return -func_LL # return the negative log likelihood
 
 
-        alpha_ji = alpha[idx]
-        hazard = np.log(alpha_ji * (t_i - t_j))
+def G_1_eta(inputDf, eta):
+    '''
+
+    :param inputDf: Dataframe containing the node information for each node
+    :param: eta: exposure rate
+    :return:
+    '''
+
+    nodeList = inputDf['node'].tolist()
+    timeList = inputDf['time'].tolist()
+
+    nodeRtTime = dict(zip(nodeList, timeList))
+    func_LL = 0. # function log likelihood
+
+    # Compute the log likelihood for each cascade
+    last = 0.
+    exposure = 0.
+    curr_cascade_funcValue = 0.
+    for idx, row in inputDf.iterrows():
+        cuurNode = row['node']
+        exposureNodes = row['exposedNodes']
+
+        t_i = row['time']
 
         # 3. Exposure term - from motif nodes
         for m in exposureNodes:
@@ -98,12 +161,11 @@ def G_1(inputDf, alpha, eta):
                 t_m = last - 1
             else:
                 t_m = nodeRtTime[m]
+            exposure += np.log(eta[nodePairs_eta[(currNode, m)]] * abs(1/(t_i - t_m)))
 
-            exposure += np.log(eta[idx] * (1/(t_i - t_m)))
         last = row['time']
 
-        curr_cascade_funcValue += survival + hazard + exposure
-
+        curr_cascade_funcValue += exposure
         func_LL += curr_cascade_funcValue
 
     return -func_LL # return the negative log likelihood
@@ -118,6 +180,7 @@ def lasso_G_2(inputDf, degDict):
     '''
 
     alpha = []
+    mid = []
     for idx, row in inputDf.iterrows():
         currNode = row['node']
         nonParents = row['nonParents']
@@ -125,7 +188,7 @@ def lasso_G_2(inputDf, degDict):
         try:
             x_i = degDict[str(currNode)]
         except:
-            x_i = np.random.uniform(0,.1,1) # sample from uniform distribution
+            x_i = int(np.random.uniform(1, 3, 1)) # sample from uniform distribution
 
         for k in nonParents:
             # parent should not be in nonParents
@@ -135,14 +198,14 @@ def lasso_G_2(inputDf, degDict):
             try:
                 x_k = degDict[str(k)]
             except:
-                x_k = np.random.uniform(0, .1, 1)  # sample from uniform distribution
+                x_k = int(np.random.uniform(1, 3, 1))  # sample from uniform distribution
 
             alpha.append([x_k * x_i])
 
     return np.array(alpha)
 
 
-def lasso_G_3(inputDf, gamma, degDict):
+def lasso_G_3(inputDf, degDict):
     '''
 
     :param inputDf: Dataframe containing the node information for each node
@@ -157,28 +220,42 @@ def lasso_G_3(inputDf, gamma, degDict):
             try:
                 x_m = degDict[str(m)]
             except:
-                x_m = np.random.uniform(0, .1, 1)  # sample from uniform distribution
+                x_m = int(np.random.uniform(1, 3, 1) ) # sample from uniform distribution
 
-            eta.append([gamma[idx] * x_m])
+            eta.append([x_m])
 
     return np.array(eta)
 
 
-
-
 def optimize_coordinate_descent(inputDf, degDict):
     maxIter = 100
-    len_input = len(inputDf)
 
-    alpha_init = np.array(random.sample(range(0.001, 0.1), len_input))
-    beta_init = np.array(random.sample(range(0.001, 0.1), len_input))
-    clf = linear_model.Lasso(alpha=0.001)
-    X = lasso_G_2(inputDf, degDict)
-    print(X)
-    # clf.fit(alpha_init, X)
-    # beta = clf.coef_
 
-    # for iter in range(maxIter):
+    # alpha_init = np.array(random.sample(range(0.001, 0.1), len_input))
+    # # beta_init = np.array(random.sample(range(0.001, 0.1), len_input))
+    # clf_G_2 = linear_model.Lasso(alpha=0.001)
+    # X_G_2 = lasso_G_2(inputDf, degDict)
+    # beta_init = np.array([random.uniform(0, 5) for _ in range(X_G_2.shape[0])])
+    #
+    # # print(X, beta_init)
+    # clf_G_2.fit(X_G_2, beta_init)
+    # beta = clf_G_2.coef_
+    # print(beta)
+
+    # clf_G_3 = linear_model.Lasso(alpha=0.001)
+    # X_G_3 = lasso_G_3(inputDf, degDict)
+    # gamma_init = np.array([random.uniform(1, 5) for _ in range(X_G_3.shape[0])])
+    #
+    # # print(X, beta_init)
+    # clf_G_3.fit(X_G_3, gamma_init)
+    # gamma = clf_G_3.coef_
+
+
+
+
+
+
+        # for iter in range(maxIter):
     #
     #     result = minimize(funcX, [inputDf])
     #     if result.success:
@@ -189,9 +266,8 @@ def optimize_coordinate_descent(inputDf, degDict):
 
 
 def main():
-    degDict = pd.read_pickle('../../../data/deg_centralities_diff_T07_08-v1.pcikle')
-    df_opt = pd.read_pickle('../../../data/df_optimize_input_sample.pickle')
-    optimize_coordinate_descent(df_opt, degDict)
+    print('Starting optimization....')
+    optimize_coordinate_descent(df_opt, outDeg)
     # print(df_opt[:10])
     # funcX(df_opt, 0.5, 0.5)
 
--- utilities/test.py
+++ utilities/test.py
@@ -1,3 +1,12 @@
+import random
+import numpy as np
 a = 1
 b = 2
 
+beta_init = random.uniform(1.5, 1.9)
+
+c = {}
+c[(1, 2)] = 4
+
+if (1, 2) in c:
+    print('hello')
